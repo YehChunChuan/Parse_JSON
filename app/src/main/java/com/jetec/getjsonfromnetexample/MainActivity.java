059205a88f48d765aca3223799528197e7db9a11
AndyYeh <andy00307most@gmail.com>
2020-11-24 15:11:09 +0800

    幹再測試!!!

--- app/src/main/java/com/jetec/getjsonfromnetexample/MainActivity.java
+++ app/src/main/java/com/jetec/getjsonfromnetexample/MainActivity.java
@@ -277,7 +277,6 @@ public class MainActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             // 什麼都不用寫
-            //12313
         }
         else {
             // 什麼都不用寫
